e37158c577ac2809ef38953556ae8edd7cbb9c55
Cameron <camlecuyer@yahoo.com>
2018-11-27 14:17:49 -0600

    Lab 4 Reasoning part

--- Lab_3/src/main/scala/Reasoning.scala
+++ Lab_3/src/main/scala/Reasoning.scala
@@ -1,4 +1,5 @@
 import org.apache.spark.{SparkConf, SparkContext}
+import scala.collection.mutable.ListBuffer
 
 object Reasoning {
   private val IN_PATH = "C:\\Users\\camle\\Box\\projectOutput\\ontTriples\\part-00000"
@@ -16,12 +17,46 @@ object Reasoning {
       val data = line.split(",").dropRight(1)
 
       (data.head, data.drop(1).head, data.drop(2).head)
-    }).toLocalIterator.toArray
+    }).cache().toLocalIterator.toArray
+
+    var reasonList: ListBuffer[String] = new ListBuffer[String]
 
     triples.foreach(ele => {
       triples.foreach(ele1 => if(ele._1.compareTo(ele1._3) == 0  && ele._3.compareTo(ele1._1) == 0 && ele._2.compareTo(ele1._2) == 0) {
-        println(ele)
+        reasonList += "Sym:" + ele._1 + "," + ele._2 + "," + ele._3 + ";" + ele1._1 + "," + ele1._2 + "," + ele1._3
       })
     })
+
+    triples.foreach(ele => {
+      triples.foreach(ele1 => if(ele._1.compareTo(ele1._3) == 0  && ele._3.compareTo(ele1._1) == 0 && ele._2.compareTo(ele1._2) != 0) {
+        reasonList += "Inv:" + ele._1 + "," + ele._2 + "," + ele._3 + ";" + ele1._1 + "," + ele1._2 + "," + ele1._3
+      })
+    })
+
+    triples.foreach(ele => {
+      triples.foreach(ele1 => if(ele._1.compareTo(ele1._3) != 0  && ele._3.compareTo(ele1._1) == 0 && ele._2.compareTo(ele1._2) == 0) {
+        reasonList += "Tra:" + ele._1 + "," + ele._2 + "," + ele._3 + ";" + ele1._1 + "," + ele1._2 + "," + ele1._3
+      })
+    })
+
+    triples.foreach(ele => {
+      triples.foreach(ele1 => if(ele._1.compareTo(ele1._3) != 0  && ele._3.compareTo(ele1._1) == 0 && ele._2.compareTo(ele1._2) != 0) {
+        reasonList += "Cha:" + ele._1 + "," + ele._2 + "," + ele._3 + ";" + ele1._1 + "," + ele1._2 + "," + ele1._3
+      })
+    })
+
+    triples.foreach(ele => {
+      triples.foreach(ele1 => if(ele._1.compareTo(ele1._3) == 0  && ele._3.compareTo(ele1._1) == 0 && ele._2.compareTo("beParent") == 0) {
+        reasonList += "Asy:" + ele._1 + "," + ele._2 + "," + ele._3 + ";" + ele1._1 + "," + ele1._2 + "," + ele1._3
+      })
+    })
+
+    triples.foreach(ele => {
+      if(ele._1.compareTo(ele._3) == 0) {
+        reasonList += "Irr:" + ele._1 + "," + ele._2 + "," + ele._3
+      }
+    })
+
+    reasonList.toList.foreach(println)
   }
 }
